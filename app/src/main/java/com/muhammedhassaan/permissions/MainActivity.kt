d8a514ca553c7b172109b71cf31f2b8eb07b037d
Muhammed <muhammed.hassaann@gmail.com>
2023-02-27 18:41:16 +0200

    applied view binding

--- app/src/main/java/com/muhammedhassaan/permissions/MainActivity.kt
+++ app/src/main/java/com/muhammedhassaan/permissions/MainActivity.kt
@@ -2,10 +2,17 @@ package com.muhammedhassaan.permissions
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.muhammedhassaan.permissions.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        
     }
 }
